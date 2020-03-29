c7db4cb3c80ee259bcaa374dc2df219e7297826d
Joseph Townshend <joetownshend@mac.com>
2020-03-29 13:52:36 +0100

    adds button to UI

--- java_GUI_project/src/java_GUI_project/GUI.java
+++ java_GUI_project/src/java_GUI_project/GUI.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -13,9 +14,12 @@ public class GUI {
 
     JFrame frame = new JFrame();
 
+    JButton button = new JButton("Click me");
+
     JPanel panel = new JPanel();
     panel.setBorder(BorderFactory.createEmptyBorder(30, 30 , 10 , 30));
     panel.setLayout(new GridLayout(0, 1));
+    panel.add(button);
 
     frame.add(panel, BorderLayout.CENTER); // add panel to frame
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // behaviour for closing frame
